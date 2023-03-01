0940666403425a80ceb08ca9445d885b4cdc32db
droxy <chiomaotu22@gmail.com>
2023-03-01 15:02:02 +0100

    set up apollo client

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Register from './pages/Register';
@@ -10,22 +11,29 @@ import Otp from './pages/ResetPassword/Otp';
 import CreatePassword from './pages/ResetPassword/CreatePassword';
 
 function App() {
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: 'https://travelbay-9vyj.onrender.com/graphql',
+  });
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/reset-password" element={<ResetPassword />}>
-          <Route path="/reset-password/password" element={<Password />} />
-          <Route path="/reset-password/otp" element={<Otp />} />
-          <Route
-            path="/reset-password/create-password"
-            element={<CreatePassword />}
-          />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/reset-password" element={<ResetPassword />}>
+            <Route path="/reset-password/password" element={<Password />} />
+            <Route path="/reset-password/otp" element={<Otp />} />
+            <Route
+              path="/reset-password/create-password"
+              element={<CreatePassword />}
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
