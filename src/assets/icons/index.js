ae422998dfddb723d31f162b7189f1cf58873650
droxy <chiomaotu22@gmail.com>
2023-03-02 02:59:19 +0100

    added avatar icon

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -3,5 +3,13 @@ import logo from './logo.svg';
 import arrowDownDark from './arrow-down.svg';
 import undoBlueIcon from './undo.svg';
 import emptyHeartIcon from './empty-heart.svg';
+import avatarIcon from './avatar.svg';
 
-export { googleIcon, logo, arrowDownDark, undoBlueIcon, emptyHeartIcon };
+export {
+  googleIcon,
+  logo,
+  arrowDownDark,
+  undoBlueIcon,
+  emptyHeartIcon,
+  avatarIcon,
+};
