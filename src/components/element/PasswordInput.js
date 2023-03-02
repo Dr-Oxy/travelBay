21011b080d0947de76f689056e3034df58e6ea9a
droxy <chiomaotu22@gmail.com>
2023-03-02 06:35:24 +0100

    refactored password component

--- src/components/element/PasswordInput.js
+++ src/components/element/PasswordInput.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PasswordInput = React.forwardRef(function Input(
-  { type, show, setShow, placeholder, disabled, ...props },
+  { placeholder, disabled, ...props },
   ref,
 ) {
+  const [show, setShow] = useState(false);
+
   return (
     <div className="password-wrapper rounded-lg flex items-center justify-between  w-full text-gray-1 text-base py-3 px-2 border border-stroke-black focus-within:border-2 focus-within:border-primary-blue">
       <input
         className="flex-1"
         ref={ref}
-        type={type}
+        type={show ? 'text' : 'password'}
         placeholder={placeholder}
         disabled={disabled}
         {...props}
